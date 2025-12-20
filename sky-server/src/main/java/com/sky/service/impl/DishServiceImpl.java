9fa3b6c6fee05dec7a2d98d0dccc60241619de44
tangtang <395121510@qq.com>
2025-12-20 20:28:19 +0800

    chore: ignore application config files

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sky.service.impl;
+
+import com.sky.service.DishService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DishServiceImpl implements DishService {
+
+
+}
