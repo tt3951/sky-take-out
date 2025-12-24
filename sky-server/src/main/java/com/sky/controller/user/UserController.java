7431f2a1a6315cd01c3957d4a374747fbd104f1d
tangtang <395121510@qq.com>
2025-12-24 20:22:12 +0800

    微信小程序用户端登陆功能

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,35 @@
+package com.sky.controller.user;
+
+import com.sky.dto.UserLoginDTO;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.vo.UserLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/user")
+@Api(tags = "C端用户相关接口")
+@Slf4j
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    @ApiOperation("微信登陆")
+    public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
+
+        log.info("微信登陆");
+        UserLoginVO userLoginVO = userService.wxlogin(userLoginDTO);
+        return Result.success(userLoginVO);
+
+    }
+}
